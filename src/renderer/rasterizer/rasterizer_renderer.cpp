0545ec5baad7e21c901e204a3d4bc96e474da5fb
Alexander <sasha.kudasov04@gmail.com>
2022-05-28 13:56:15 +0300

    depth in rasterizer

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -9,7 +9,6 @@ void cg::renderer::rasterization_renderer::init()
 			settings->width, settings->height);
 	rasterizer = std::make_shared<cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
 	rasterizer->set_viewport(settings->width, settings->height);
-	rasterizer->set_render_target(render_target);
 
 	model = std::make_shared<cg::world::model>();
 	model->load_obj(settings->model_path);
@@ -26,7 +25,10 @@ void cg::renderer::rasterization_renderer::init()
 	camera->set_angle_of_view(settings->camera_angle_of_view);
 	camera->set_z_near(settings->camera_z_near);
 	camera->set_z_far(settings->camera_z_far);
-	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
+
+	depth_buffer = std::make_shared<cg::resource<float>>(
+			settings->width, settings->height);
+	rasterizer->set_render_target(render_target, depth_buffer);
 }
 void cg::renderer::rasterization_renderer::render()
 {
