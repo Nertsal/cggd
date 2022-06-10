185a432a5eaa28797aeee2063aaabfc7cc4def4c
Alexander <sasha.kudasov04@gmail.com>
2022-06-10 13:23:29 +0300

    Adjust `trace_ray` method of `raytracer` to use `any_hit_shader`

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -226,6 +226,10 @@ namespace cg::renderer
 				// Current triangle is closer
 				closest_hit_payload = payload;
 				closest_triangle = &triangle;
+
+				if (any_hit_shader) {
+					return any_hit_shader(ray, payload, triangle);
+				}
 			}
 		}
 
@@ -236,8 +240,6 @@ namespace cg::renderer
 		}
 
 		return miss_shader(ray);
-
-		// TODO: Lab 2.04. Adjust `trace_ray` method of `raytracer` to use `any_hit_shader`
 		// TODO: Lab 2.05. Adjust trace_ray method of raytracer class to traverse the acceleration structure
 	}
 
